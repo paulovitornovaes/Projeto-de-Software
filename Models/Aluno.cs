d38e9d2cf0fe66a6b78f3d1d67c5b56f2a79f91d
Paulo Vitor <paulonovaes@id.uff.br>
2024-04-09 18:41:59 -0300

    adicao de aluno

---
+++ Models/Aluno.cs
@@ -0,0 +1,7 @@
+﻿namespace Iduff.Models;
+
+public class Aluno : Usuario
+{
+    public long matricula { get; set; }
+    public List<Certificado> Certificados { get; set; }
+}
---
+++ Services/CertificadoService.cs
@@ -0,0 +1,11 @@
+﻿using Iduff.Models;
+
+namespace Iduff.Services;
+
+public class CertificadoService
+{
+    public int ObterQuantidadeHorasTotal(Usuario usuario)
+    {
+        return 0;
+    }
+}

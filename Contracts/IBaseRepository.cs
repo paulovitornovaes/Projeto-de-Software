a1566d529fce445ebd0963d49770875a8db89b78
Paulo Vitor <paulonovaes@id.uff.br>
2024-04-23 20:37:06 -0300

    adicao do padrao BaseRepository.cs

---
+++ Contracts/IBaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Iduff.Contracts;
+
+public interface IBaseRepository<T> where T : class
+{
+    Task<IEnumerable<T>> ListAllAsync();
+    Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> filter);
+    IQueryable<T> GetAll(Expression<Func<T, bool>> filter);
+    Task<T> GetByIdAsync(int id);
+    Task<T> GetByAsync(Expression<Func<T, bool>> filter);
+    Task<T> CreateAsync(T t);
+    T Create(T t);
+    Task SaveAsync();
+    Task<T> CreateAndSaveAsync(T t);
+    T Update(T t);
+    Task AddRangeAsync(List<T> t);
+    Task UpdateAndSaveAsync(T t);
+    Task DeleteAsync(int id);
+    IQueryable<T> GetQueryable();
+    IEnumerable<T> Get(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = "");
+    IQueryable<T> Include(params Expression<Func<T, object>>[] includes);
+    Task<DateTime> GetMaxDtAtualizacao();
+}
---
+++ Repositories/BaseRepository.cs
@@ -0,0 +1,143 @@
+﻿using System.Linq.Expressions;
+using Iduff.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Iduff.Repositories;
+
+public class BaseRepository<T> : IBaseRepository<T> where T : class
+{
+    protected readonly DbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    public BaseRepository(DbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public async Task<IEnumerable<T>> ListAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> filter)
+    {
+        return await _dbSet.Where(filter).ToListAsync();
+    }
+
+    public IQueryable<T> GetAll(Expression<Func<T, bool>> filter)
+    {
+        return _dbSet.Where(filter);
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<T> GetByAsync(Expression<Func<T, bool>> filter)
+    {
+        return await _dbSet.FindAsync(filter);
+    }
+
+    public async Task<T> CreateAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        return entity;
+    }
+
+    public T Create(T entity)
+    {
+        _dbSet.Add(entity);
+        return entity;
+    }
+
+
+    public async Task<T> CreateAndSaveAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        await SaveAsync();
+        return entity;
+    }
+
+    public async Task SaveAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public T Update(T entity)
+    {
+        _dbSet.Update(entity);
+        return entity;
+    }
+
+    public async Task UpdateAndSaveAsync(T entity)
+    {
+        _dbSet.Update(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task AddRangeAsync(List<T> entity)
+    {
+        await _dbSet.AddRangeAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var entity = await _dbSet.FindAsync(id);
+        if (entity != null)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public IQueryable<T> GetQueryable()
+    {
+        return _dbSet.AsQueryable();
+    }
+    
+    public virtual IEnumerable<T> Get(
+        Expression<Func<T, bool>> filter = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+        string includeProperties = "")
+    {
+        IQueryable<T> query = _dbSet;
+
+        if (filter != null)
+        {
+            query = query.Where(filter);
+        }
+
+        foreach (var includeProperty in includeProperties.Split
+                     (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+        {
+            query = query.Include(includeProperty);
+        }
+
+        if (orderBy != null)
+        {
+            return orderBy(query).ToList();
+        }
+        else
+        {
+            return query.ToList();
+        }
+    }
+
+    public IQueryable<T> Include(params Expression<Func<T, object>>[] includes)
+    {
+        IQueryable<T> query = _dbSet;
+        foreach (var include in includes)
+            query = query.Include(include);
+        return query;
+    }
+
+    public async Task<DateTime> GetMaxDtAtualizacao()
+    {
+        var parameter = Expression.Parameter(typeof(T), "p");
+        var property = Expression.Property(parameter, "DtAtualizacao");
+        var selector = Expression.Lambda<Func<T, DateTime>>(property, parameter);
+        return await _dbSet.Select(selector).MaxAsync(); }
+}

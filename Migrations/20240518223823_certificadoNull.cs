5ecac023da4d369f23f112d0edbd302f1c4b160f
Paulo Vitor <paulonovaes@id.uff.br>
2024-05-18 19:38:55 -0300

    migrations null

---
+++ Migrations/20240518223823_certificadoNull.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Iduff.Migrations
+{
+    /// <inheritdoc />
+    public partial class certificadoNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NomePalestrante",
+                table: "Certificados",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NomePalestrante",
+                table: "Certificados",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}

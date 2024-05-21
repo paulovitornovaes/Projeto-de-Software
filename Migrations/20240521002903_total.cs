a206b49461d436bd14d37948dda69dc10e244e10
Paulo Vitor <paulonovaes@id.uff.br>
2024-05-20 21:47:31 -0300

    migracoes

---
+++ Migrations/20240521002903_total.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Iduff.Migrations
+{
+    /// <inheritdoc />
+    public partial class total : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "total",
+                table: "CargaHoraria",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "total",
+                table: "CargaHoraria");
+        }
+    }
+}

2e9d866e9dd54bf0593ea185fe4c73b7d6440580
Paulo Vitor <paulonovaes@id.uff.br>
2024-05-20 22:40:24 -0300

    migracoes

---
+++ Migrations/20240521011933_ch.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Iduff.Migrations
+{
+    /// <inheritdoc />
+    public partial class ch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_CargaHorariaId",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "organizacoes",
+                table: "CargaHoraria",
+                newName: "organizarPalestras");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_CargaHorariaId",
+                table: "AspNetUsers",
+                column: "CargaHorariaId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_CargaHorariaId",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "organizarPalestras",
+                table: "CargaHoraria",
+                newName: "organizacoes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_CargaHorariaId",
+                table: "AspNetUsers",
+                column: "CargaHorariaId");
+        }
+    }
+}

5c78afa4b5c90e965b3a9fa0ace73b3a4129baa4
Paulo Vitor <paulonovaes@id.uff.br>
2024-05-21 18:48:19 -0300

    refatorando

--- Services/EventoService.cs
+++ Services/EventoService.cs
@@ -93,7 +93,6 @@ public class EventoService : IEventoService
         await _context.SaveChangesAsync();
         return evento;
     }
-
     
     public async Task SalvaPresencaEvento(IFormFile arquivo, Evento evento)
     {
